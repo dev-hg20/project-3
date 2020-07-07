823d3241fd03f1c1f66b8f083121613dc5006e07
Haylie Goh <h.goh@deakin.edu.au>
2020-07-07 23:20:10 +1000

    initial upload

---
+++ controllers/booksController.js
@@ -0,0 +1,34 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function (req, res) {
+    db.Book.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // findById: function (req, res) {
+  //   db.Book.findById(req.params.id)
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+  create: function ({ body: { title, author, description, image, link } }) {
+    db.Book.create({
+      title: title,
+      author: author,
+      description: description,
+      image: image,
+      link: link,
+    })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+
+  // remove: function (req, res) {
+  //   db.Book.findById({ _id: req.params.id })
+  //     .then((dbModel) => dbModel.remove())
+  //     .then((dbModel) => res.json(dbModel))
+  //     .catch((err) => res.status(422).json(err));
+  // },
+};
